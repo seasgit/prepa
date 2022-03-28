8e8dc40dcdd57bf9b56e49fa7522274ce18c0fab
seasone <contact.seasone@gmail.com>
2022-03-28 10:49:39 +0200

    Digital Campus cours des prepa

--- cours_javascript/exercices/scroll_menu/js/app.js
+++ cours_javascript/exercices/scroll_menu/js/app.js
@@ -3,58 +3,57 @@
 const ancres = document.querySelectorAll('.menu a')
 const navbar = document.querySelector('.navbar')
 const bouton = document.querySelector('.bouton-croix')
-const croix =  bouton.querySelector('span')
+const croix = bouton.querySelector('span')
+
+// A l'ouverture, si min-width:768px alors navbar visible
+
+if (window.matchMedia("(min-width:768px)").matches) {
+    navbar.style.display = 'flex'
+} else {
+    navbar.style.display = 'none'
+    open = false;
+    croix.style.transform = "rotate(45deg)"
+}
 
 /*
-A l'ouverture si on est en mobile :
-    le menu est fermé et le bouton affiche une croix
-*/
+    Au clic
 
-let open = false
-croix.style.transform =  "rotate(45deg)"
-navbar.style.display ='none'
-
-
-/*
-    Au clic sur le bouton
-        - si le menu  n'est pas visible
-            display flex
-          sinon 
-            display none
-        - on change la valeur de open ( interrupteur true/false)
-            selon la valeur de open => rotation du bouton
 */
 
 bouton.addEventListener('click', (e) => {
-    if( open == false ){
-        navbar.style.display = 'flex'
-        croix.style.transform = "rotate(0deg)"
-    }else{
-        navbar.style.display = 'none'
-        croix.style.transform = "rotate(45deg)"
+
+    if (window.matchMedia("(max-width:767px)").matches) {
+
+        if (open == false) {
+            navbar.style.display = 'flex'
+            croix.style.transform = "rotate(0deg)"
+        } else {
+            navbar.style.display = 'none'
+            croix.style.transform = "rotate(45deg)"
+        }
+        open = !open
     }
-    open = !open
 
 })
 
 
-ancres.forEach( ancre => {
+ancres.forEach(ancre => {
     ancre.addEventListener('click', () => {
         open = true;
         bouton.click()
     })
-} )
+})
 
 
 window.addEventListener('resize', redimensionnement)
 
 function redimensionnement() {
 
-    if(window.matchMedia("(min-width:768px)").matches) {
+    if (window.matchMedia("(min-width:768px)").matches) {
         navbar.style.display = 'flex'
-    }else{
+    } else {
         navbar.style.display = 'none'
-        open= false;
+        open = false;
         croix.style.transform = "rotate(45deg)"
     }
 
