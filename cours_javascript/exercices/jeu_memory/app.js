baf8914c577e33b15bc96a2442f1cf34c849abac
seasone <contact.seasone@gmail.com>
2022-03-29 10:13:48 +0200

    Prepa cours JS

---
+++ cours_javascript/exercices/jeu_memory/app.js
@@ -0,0 +1,9 @@
+/*
+    Références des éléments du DOM
+*/
+
+const countdown =  document.querySelector('.countdown')
+const btnJouer =  document.querySelector('.btn-play')
+const info =  document.querySelector('.info')
+const zoneJeu =  document.querySelector('.zone-jeu')
+
